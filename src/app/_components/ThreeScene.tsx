befde4c55a9b29e045fe8cd514a228a41f2189fa
Kolbe Weathington <kolbe.weathington@gmail.com>
2023-12-04 11:13:21 -0600

    ...

--- src/app/_components/ThreeScene.tsx
+++ src/app/_components/ThreeScene.tsx
@@ -1,58 +1,77 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 'use client'
-import React, { useRef, useEffect, useMemo, useState } from 'react';
+import React, { useRef, useEffect } from 'react';
 import * as THREE from 'three';
-import { GLTF, GLTFLoader } from 'three/examples/jsm/Addons.js';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+
+const setupInitalSceneControls = (containerRef: React.RefObject<HTMLDivElement>) => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.OrthographicCamera(window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, -20000, 20000);
+
+    // camera.position.set(1, 1, 1);
+    camera.quaternion.setFromEuler(new THREE.Euler(-1.57, 0, 0));
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    containerRef.current?.appendChild(renderer.domElement);
+    // camera.position.z = 1;
+
+    function onWindowResize() {
+        camera.left = window.innerWidth / - 2;
+        camera.right = window.innerWidth / 2;
+        camera.top = window.innerHeight / 2;
+        camera.bottom = window.innerHeight / - 2;
+        camera.zoom = 100;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    window.addEventListener('resize', onWindowResize);
+
+    return { scene, camera, renderer, onWindowResize }
+}
+
+// const loadSixLight = () => {
+//     const loader = new GLTFLoader();
+//     let model: THREE.Group<THREE.Object3DEventMap> | undefined
+//     loader.loadAsync(
+//         // resource URL
+//         '/models/gltf/light.gltf',
+//         // called when the resource is loaded
+//         function (progress) {
+//             // setLogoRef(gltf)
+//             console.log(progress)
+//         }).then((gltf) => {
+//             model = gltf.scene
+//         }).catch((err) => console.log(err))
+
+//     return model;
+// }
+
 
 const ThreeScene: React.FC = () => {
     const containerRef = useRef<HTMLDivElement>(null);
-    const [logoRef, setLogoRef] = useState<GLTF>();
-
-    const setupInitalSceneControls = () => {
-        const scene = new THREE.Scene();
-        const camera = new THREE.OrthographicCamera(window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, -20000, 20000);
-
-
-        // camera.position.set(1, 1, 1);
-        camera.quaternion.setFromEuler(new THREE.Euler(-1.57, 0, 0));
-
-        const renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        containerRef.current?.appendChild(renderer.domElement);
-        // camera.position.z = 1;
-
-        function onWindowResize() {
-            camera.left = window.innerWidth / - 2;
-            camera.right = window.innerWidth / 2;
-            camera.top = window.innerHeight / 2;
-            camera.bottom = window.innerHeight / - 2;
-            camera.zoom = 150;
-            camera.updateProjectionMatrix();
-            renderer.setSize(window.innerWidth, window.innerHeight);
-        }
-
-        window.addEventListener('resize', onWindowResize);
-
-        return { scene, camera, renderer, onWindowResize }
-    }
 
     useEffect(() => {
         if (typeof window !== 'undefined') {
-            const { scene, camera, renderer, onWindowResize } = setupInitalSceneControls()
+            const { scene, camera, renderer, onWindowResize } = setupInitalSceneControls(containerRef)
 
             const geometry = new THREE.BoxGeometry();
             const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
             const cube = new THREE.Mesh(geometry, material);
             const loader = new GLTFLoader();
 
+            let light: THREE.Group<THREE.Object3DEventMap>;
+            let logo: THREE.Group<THREE.Object3DEventMap>;
+
             loader.load(
                 // resource URL
-                '/models/gltf/sixEncolsedLogo.gltf',
+                '/models/gltf/6ixLogoF.gltf',
                 // called when the resource is loaded
                 function (gltf) {
-                    setLogoRef(gltf)
-                    scene.add(gltf.scene)
-                    console.log(gltf.cameras)
+                    // setLogoRef(gltf)
+                    logo = gltf.scene
+                    scene.add(logo)
                     gltf.animations; // Array<THREE.AnimationClip>
                     gltf.scene; // THREE.Group
                     gltf.scenes; // Array<THREE.Group>
@@ -69,16 +88,31 @@ const ThreeScene: React.FC = () => {
 
                 })
 
-            // scene.add(cube);
+            loader.load(
+                // resource URL
+                '/models/gltf/light.gltf',
+                // called when the resource is loaded
+                function (gltf) {
+                    // setLogoRef(gltf)
+                    light = gltf.scene
+                    scene.add(light)
+                    gltf.animations; // Array<THREE.AnimationClip>
+                    gltf.scene; // THREE.Group
+                    gltf.scenes; // Array<THREE.Group>
+                    gltf.cameras; // Array<THREE.Camera>
+                    gltf.asset
+                },
+                // called while loading is progressing
+                function (xhr) {
+                    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+                },
+                // called when loading has errors
+                function (error) {
+                    console.log('An error happened');
 
-            if (logoRef) {
-                scene.add(logoRef.scene);
-                logoRef.animations; // Array<THREE.AnimationClip>
-                logoRef.scene; // THREE.Group
-                logoRef.scenes; // Array<THREE.Group>
-                logoRef.cameras; // Array<THREE.Camera>
-                logoRef.asset; // Object
-            }
+                })
+
+            scene.add(cube);
 
             renderer.render(scene, camera);
             // Add this function inside the useEffect hook
@@ -86,7 +120,8 @@ const ThreeScene: React.FC = () => {
                 cube.rotation.x += 0.01;
                 cube.rotation.y += 0.01;
                 cube.position.x += 1;
-                // console.log(cube.position.x)
+                if (!!logo)
+                    logo.rotation.z += 0.01;
                 renderer.render(scene, camera);
                 requestAnimationFrame(renderScene);
             };
@@ -94,10 +129,6 @@ const ThreeScene: React.FC = () => {
             // Call the renderScene function to start the animation loop
             renderScene();
             onWindowResize();
-
-            return () => {
-                window.removeEventListener('resize', onWindowResize);
-            };
         }
     }, []);
 
