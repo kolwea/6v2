6bd60ce17648f1f6258aa5d639c2bc98ca23fc42
Kolbe Weathington <kolbe.weathington@gmail.com>
2023-12-05 12:09:51 -0600

    Tailwind Libraries ...

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,5 @@
+import { founders } from "../layout"
+export const SixDegreeHeader = () => {
+    return <div className={`font-sans ${founders.className}`}>the6ixCollective</div>
+
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { SixDegreeHeader } from "./_components/Header";
+import Example from "./_components/TailwindExample";
 import ThreeScene from "./_components/ThreeScene";
+
 
 export default async function Home() {
   return (
-    <main className="flex flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <ThreeScene />
+    <main className="flex flex-col items-center justify-center">
+      <ThreeScene >
+        <SixDegreeHeader />
+      </ThreeScene>
     </main>
   );
 }
