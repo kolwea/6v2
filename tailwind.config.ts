fc372faebf19421b9a8ad1197d8d6d96636bcaf4
Kolbe Weathington <kolbe.weathington@gmail.com>
2023-12-12 10:01:31 -0600

    NextUI cont...

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,15 @@ import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: [
+    "./src/**/*.tsx",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
-        founders: ["var(--font-founders)"]
+        founders: ["var(--font-founders)"],
       },
     },
   },
