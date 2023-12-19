e5165fb98397f1dc1fad961f9a21fda5d4225c05
Kolbe Weathington <kolbe.weathington@gmail.com>
2023-12-19 10:28:23 -0600

    ...

--- src/app/_components/JoinMailingList.tsx
+++ src/app/_components/JoinMailingList.tsx
@@ -14,7 +14,6 @@ export const SignupModal = () => {
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         useJoinMailinglist.mutate({ name, email })
-        // onClose()
     }
 
     return (
