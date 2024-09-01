b3821b3f31ab65dc53dccdf7f02a5c7d86721a67
Kolbe Weathington <kolbe.weathington@gmail.com>
2024-09-01 18:22:17 -0500

    ...

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,3 +1,4 @@
+import { warn } from "console";
 import { z } from "zod";
 import { env } from "~/env";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
@@ -22,21 +23,12 @@ export const userRouter = createTRPCRouter({
             last_name: input.last_name,
           },
         },
-      });
-    }),
-  verificationCheck: publicProcedure
-    .input(
-      z.object({
-        code: z.string().length(6),
-        to: z.string(),
-      }),
-    )
-    .query(async ({ ctx, input }) => {
-      const check = await ctx.twilioClient.verify.v2
-        .services(env.TWILIO_SERVICE_ID)
-        .verificationChecks.create(input);
-
-      console.log(check);
-      return check;
+      })
+      
+      console.log(data)
+      if(error){
+        warn(error)
+      }
+      return data
     }),
 });
